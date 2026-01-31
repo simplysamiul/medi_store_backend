18077d10a24ace7e0cf8aa63596b305efb8fafbf
simplysamiul <simplysamiul@gmail.com>
2026-01-31 23:21:07 +0600

    server created and running successfully

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,18 @@
-import express from 'express';
+import express, { Application } from 'express';
 import cors from 'cors'
 
-const app = express();
+const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+
+// root route
+app.get("/", (req, res) => {
+    res.send("Welcome to MediStore Backend!");
+});
+
+
+
 
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,23 @@
 import app from "./app";
+import { prisma } from "./lib/prisma";
 
 const PORT = process.env.PORT || 5000;
 
 
-export default function server() {
-  app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-  });
-}
+export default async function server() {
+  try {
+    await prisma.$connect();
+    console.log("Database connected successfully");
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Failed to connect to the database:", error);
+    await prisma.$disconnect();
+    process.exit(1);
+  }
+};
+
+
+server();
